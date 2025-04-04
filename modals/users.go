771da3897a239bfde0546e866521ad7d6e897d1b
userlll1986 <lilinhu2008@qq.com>
2025-04-04 12:44:57 +0800

    mysql插件

---
+++ modals/users.go
@@ -0,0 +1,9 @@
+package modals
+
+// User 结构体声明
+type User struct {
+	UserId    int64  `gorm:"primaryKey;autoIncrement"`
+	UserName  string `gorm:"not null;type:varchar(32)"`
+	UserPwd   string `gorm:"not null;type:varchar(128)"`
+	UserPhone string `gorm:"unique;type:varchar(32)"`
+}
